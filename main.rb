ccd354d78b938411f2a662f9a90e3eab3a0801f2
Michał Zając <michal.zajac@gmail.com>
2026-03-14 05:51:29 +0100

    Use #content instead of #to_s

--- main.rb
+++ main.rb
@@ -19,7 +19,7 @@ LOGGER = SemanticLogger['bot']
 HTTP_REGEX = URI::DEFAULT_PARSER.make_regexp(%w[http https])
 
 BOT.message(contains: HTTP_REGEX) do |event|
-  message = event.message.to_s
+  message = event.message.content
 
   uri_in_message = message.match(HTTP_REGEX).to_s
   uri = URI.parse(uri_in_message)
